389ff9dc69856662c4bd576ead81b17a0495ba94
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-01-11 13:10:25 +1100

    add db schema

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS hr_db;
+
+USE hr_db;
+
+CREATE TABLE IF NOT EXISTS departments (
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+    is_removed boolean NOT NULL DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+	id int AUTO_INCREMENT,
+	title varchar(255) NOT NULL,
+    default_salary FLOAT NOT NULL, 
+    is_removed boolean NOT NULL DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS employees (
+	id int AUTO_INCREMENT,
+	first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    role_id int NOT NULL,
+    department_id int NOT NULL, 
+    salary float NOT NULL,
+    manager_id int,
+    is_removed boolean NOT NULL DEFAULT false,
+	PRIMARY KEY (id),
+    FOREIGN KEY(role_id) REFERENCES roles(id),
+    FOREIGN KEY(department_id) REFERENCES departments(id),
+    FOREIGN KEY(manager_id) REFERENCES employees(id) ON DELETE NO ACTION ON UPDATE NO ACTION 
+);
